3fb1ec003128153bf6ff6f878d4b4c0ab8762dcf
Tiago Martins <tiagocps96@hotmail.com>
2023-10-01 16:18:26 -0300

    correção validator e javascript

--- menu.js
+++ menu.js
@@ -3,3 +3,6 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem',window.scrollY > 0)
 })
+document.getElementById("botao").addEventListener("click", function() {
+    window.location.href = "https://www.google.com.br";
+});
