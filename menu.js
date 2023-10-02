b3b8fe3026f767b544dbd74bfcdbe5b3622473b5
Tiago Martins <tiagocps96@hotmail.com>
2023-10-02 20:37:06 -0300

    novamente atualizei a responsividade do site, pequenas correções nas medias query

--- menu.js
+++ menu.js
@@ -3,6 +3,11 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem',window.scrollY > 0)
 })
-document.getElementById("botao").addEventListener("click", function() {
-    window.location.href = "https://wa.me/5519997711979";
-});
+        const botaoWhatsapp = document.getElementById("botao");
+        const numeroTelefone = "5519997711979";
+        const mensagemPredefinida = "Olá, eu gostaria de agendar meu horário";
+        const urlWhatsapp = `https://api.whatsapp.com/send?phone=${numeroTelefone}&text=${encodeURIComponent(mensagemPredefinida)}`;
+        botaoWhatsapp.addEventListener("click", function() {
+            window.location.href = urlWhatsapp;
+        });
+    
